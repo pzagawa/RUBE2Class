2ae778cecb0cd78ed0ea0ef898633c05accdbf42
Piotr <piotr@Piotrs-Mac-mini.local>
2014-02-17 23:02:57 +0100

    Initial Commit

---
+++ RUBE2Class/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RUBE2Class
+//
+//  Created by Piotr on 17.02.2014.
+//  Copyright (c) 2014 Piotr Zagawa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
